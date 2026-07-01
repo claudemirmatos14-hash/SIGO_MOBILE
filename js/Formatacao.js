ce01de47fe215780a33221206a3dee1d916b8749
claudemirmatos14-hash <claudemirmatos14@gmail.com>
2026-07-01 17:24:01 -0300

    Update Formatacao.js

--- js/Formatacao.js
+++ js/Formatacao.js
@@ -1,1 +1,10 @@
+function formatarDataHoraMedicao_(valor) {
+  if (!valor) return "--";
+
+  try {
+    return new Date(valor).toLocaleString("pt-BR");
+  } catch (erro) {
+    return valor;
+  }
+}
 
