8ffdb0962779c30aea73cf874cf63c40332b0e74
claudemirmatos14-hash <claudemirmatos14@gmail.com>
2026-07-08 11:47:20 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8692,8 +8692,8 @@ window.formatarDataNotificacao_ = function (dataISO) {
 // =====================================================
 
 window.SIGO_NOTIFICACOES_LIMPEZA_CONFIG = Object.freeze({
-  diasPadrao: 0,
-  diasPrioridadeAlta: 0
+  diasPadrao: 30,
+  diasPrioridadeAlta: 90
 });
 
 window.notificacaoElegivelParaLimpezaSIGO_ = function (
