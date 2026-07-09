eed4229556d89fe472e46461fae38eb56d74fd6f
claudemirmatos14-hash <claudemirmatos14@gmail.com>
2026-07-09 19:57:34 -0300

    Eliminar duplicidade de mecanismos da fila

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // =====================================================
 
 const SIGO_CACHE_VERSION =
-  "sigo-mobile-v7";
+  "sigo-mobile-v8";
 
 const SIGO_BASE_URL =
   new URL("./", self.location.href);
