d9749bb80e120530b388d81bfd1b329a0f949ab6
claudemirmatos14-hash <claudemirmatos14@gmail.com>
2026-07-09 21:00:13 -0300

    Filtrar itens pelo Diário ativo

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // =====================================================
 
 const SIGO_CACHE_VERSION =
-  "sigo-mobile-v9";
+  "sigo-mobile-v10";
 
 const SIGO_BASE_URL =
   new URL("./", self.location.href);
