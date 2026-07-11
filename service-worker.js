cc4ee758c832f5b5b793fc69bba76469df114840
claudemirmatos14-hash <claudemirmatos14@gmail.com>
2026-07-11 09:01:39 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // =====================================================
 
 const SIGO_CACHE_VERSION =
-  "sigo-mobile-v33";
+  "sigo-mobile-v34";
 
 const SIGO_BASE_URL =
   new URL("./", self.location.href);
